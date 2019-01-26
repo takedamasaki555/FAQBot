f53ac7262d5bfd7245bc5e8ee6b7c5eba5828cb7
Masaki Takeda <masakit@microsoft.com>
2019-01-26 21:59:41 +0900

    カテゴリ変更

--- Models/FAQCategory.cs
+++ Models/FAQCategory.cs
@@ -6,7 +6,7 @@ namespace FAQCategory
     public class FAQCategory
     {
         public static List<string> CategoryList = new List<string>() {
-               
+                "カテゴリ1", "カテゴリ2", "カテゴリ3","カテゴリ4","カテゴリ選択しない"
         };
     }
 }
